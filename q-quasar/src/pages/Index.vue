7287dde3b86467406019a649063f30902bde47dc
SAI KWRN KHAM <kwarnkham.moon@gmail.com>
2019-07-29 23:02:06 +0630

    commit

---
+++ q-quasar/src/pages/Index.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:8000/api";
+export default {
+  name: "PageIndex",
+  data: () => ({
+    token: "5VpoMu5pzFi3x39fZ4UElIjNaB5Ce7COAHnoTDDyEepypY0omo8LUDF6Gtgp"
+  }),
+  methods: {
+    testApi(method) {
+      axios({
+        method: method,
+        url: "/test"
+      }).then(response => {
+        console.log(response);
+      });
+    },
+    getUser() {
+      axios({
+        method: "get",
+        url: "/user",
+        headers: {
+          Authorization: "Bearer " + this.token
+        }
+        // params: {
+        //   api_token: this.token
+        // }
+      }).then(response => {
+        console.log(response);
+      });
+    }
+  },
+  created() {
+    this.getUser();
+  }
+};
+</script>
